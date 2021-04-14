2eda16c717fec93dadabde97d13139bf9d069aaa
shubhambisht1234 <shubhambisht0123@gmail.com>
2021-04-14 13:35:32 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,19 @@
+
+function asd(){
+	var a = document.getElementById("change");
+	if(a.style.display == "block")
+	{
+	a.style.display = "none";
+	}
+
+	else
+	{
+		a.style.display = "block";
+
+	}
+}
+
+
+
+		
+	
